91d86dab7bd032c619c16ff11cdab06e08f46b7e
DeviousMelon <lolwright02@gmail.com>
2025-08-05 13:11:24 +0100

    Fix

--- src/Console.jsx
+++ src/Console.jsx
@@ -1,6 +1,5 @@
-/* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import WhoAmI from "./WhoAmI";
 import Projects from "./Projects";
 import Contact from "./Contact";
